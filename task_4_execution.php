3b10afa3113aebace5a29b33df79b74837c2dced
Киренский Данила <80262480+KirenskiyDanila@users.noreply.github.com>
2022-03-27 22:54:10 +0300

    Update task_4_execution.php

--- task_4_execution.php
+++ task_4_execution.php
@@ -1,10 +1,14 @@
 <?php
 $text = $_POST['text'];
+// подходит ли ссылка под формат
 if (!preg_match("/http:\/\/asozd\.duma\.gov\.ru\/main\.nsf\/\(Spravka\)\?OpenAgent&RN=[0-9-]+&[0-9]+/", $text)) {
     echo "Ошибка! Неверный формат!";
     die();
 }
+// помещаем числа в массив
 preg_match_all("/[0-9-]+/", $text, $lawAndNumber);
+// достаем номер закона
 $law = $lawAndNumber[0][0];
+// создаем новую ссылку
 $newLink = "http://sozd.parlament.gov.ru/bill/" . $law;
 echo $newLink;
