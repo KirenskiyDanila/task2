6d44100abf49472cc224e33c7741df9f4bc05352
Киренский Данила <80262480+KirenskiyDanila@users.noreply.github.com>
2022-03-27 22:52:47 +0300

    Update task_3_execution.php

--- task_3_execution.php
+++ task_3_execution.php
@@ -1,16 +1,16 @@
 <?php
 $text = $_POST['text'];
-
+// ищем числовые значения в кавычках и помещаем их в массив
 preg_match_all("/['][0-9]+[']/", $text, $group);
 $newGroup = array();
 
-
+// удваиваем числовое значение в кавычках
 foreach ($group[0] as $item) {
     $item = trim($item, "'");
     $value = $item * 2;
     $newGroup[] = "'" . $value . "'";
 }
-
+// обратный цикл с заменой значений в кавычках
 for ($i = count($newGroup) - 1; $i >= 0; $i--) {
     $count = $i + 1;
     $text = preg_replace("/['][0-9]+[']/", $newGroup[$i], $text, $count);
